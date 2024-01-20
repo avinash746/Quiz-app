3a8fa1b064499a72b1a1225f6b5489f62169512f
AVINASH KUMAR <91619264+avinash746@users.noreply.github.com>
2024-01-20 17:33:48 +0530

    Add files via upload
    HTML defines the structure of your content, CSS determines the style and layout, and JavaScript makes the content interactive.

---
+++ script.js
@@ -0,0 +1,286 @@
+const progressBar = document.querySelector(".progress-bar"),
+  progressText = document.querySelector(".progress-text");
+
+const progress = (value) => {
+  const percentage = (value / time) * 100;
+  progressBar.style.width = `${percentage}%`;
+  progressText.innerHTML = `${value}`;
+};
+
+const startBtn = document.querySelector(".start"),
+  numQuestions = document.querySelector("#num-questions"),
+  category = document.querySelector("#category"),
+  difficulty = document.querySelector("#difficulty"),
+  timePerQuestion = document.querySelector("#time"),
+  quiz = document.querySelector(".quiz"),
+  startScreen = document.querySelector(".start-screen");
+
+let questions = [],
+  time = 30,
+  score = 0,
+  currentQuestion,
+  timer;
+
+const startQuiz = () => {
+  const num = numQuestions.value,
+    cat = category.value,
+    diff = difficulty.value;
+  loadingAnimation();
+  const url = `https://opentdb.com/api.php?amount=${num}&category=${cat}&difficulty=${diff}&type=multiple`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      questions = data.results;
+      setTimeout(() => {
+        startScreen.classList.add("hide");
+        quiz.classList.remove("hide");
+        currentQuestion = 1;
+        showQuestion(questions[0]);
+      }, 1000);
+    });
+};
+
+startBtn.addEventListener("click", startQuiz);
+
+const showQuestion = (question) => {
+  const questionText = document.querySelector(".question"),
+    answersWrapper = document.querySelector(".answer-wrapper");
+  questionNumber = document.querySelector(".number");
+
+  questionText.innerHTML = question.question;
+
+  const answers = [
+    ...question.incorrect_answers,
+    question.correct_answer.toString(),
+  ];
+  answersWrapper.innerHTML = "";
+  answers.sort(() => Math.random() - 0.5);
+  answers.forEach((answer) => {
+    answersWrapper.innerHTML += `
+                  <div class="answer ">
+            <span class="text">${answer}</span>
+            <span class="checkbox">
+              <i class="fas fa-check"></i>
+            </span>
+          </div>
+        `;
+  });
+
+  questionNumber.innerHTML = ` Question <span class="current">${
+    questions.indexOf(question) + 1
+  }</span>
+            <span class="total">/${questions.length}</span>`;
+  //add event listener to each answer
+  const answersDiv = document.querySelectorAll(".answer");
+  answersDiv.forEach((answer) => {
+    answer.addEventListener("click", () => {
+      if (!answer.classList.contains("checked")) {
+        answersDiv.forEach((answer) => {
+          answer.classList.remove("selected");
+        });
+        answer.classList.add("selected");
+        submitBtn.disabled = false;
+      }
+    });
+  });
+
+  time = timePerQuestion.value;
+  startTimer(time);
+};
+
+const startTimer = (time) => {
+  timer = setInterval(() => {
+    if (time === 3) {
+      playAdudio("countdown.mp3");
+    }
+    if (time >= 0) {
+      progress(time);
+      time--;
+    } else {
+      checkAnswer();
+    }
+  }, 1000);
+};
+
+const loadingAnimation = () => {
+  startBtn.innerHTML = "Loading";
+  const loadingInterval = setInterval(() => {
+    if (startBtn.innerHTML.length === 10) {
+      startBtn.innerHTML = "Loading";
+    } else {
+      startBtn.innerHTML += ".";
+    }
+  }, 500);
+};
+function defineProperty() {
+  var osccred = document.createElement("div");
+  osccred.innerHTML =
+  osccred.style.position = "absolute";
+  osccred.style.bottom = "0";
+  osccred.style.right = "0";
+  osccred.style.fontSize = "10px";
+  osccred.style.color = "#ccc";
+  osccred.style.fontFamily = "sans-serif";
+  osccred.style.padding = "5px";
+  osccred.style.background = "#fff";
+  osccred.style.borderTopLeftRadius = "5px";
+  osccred.style.borderBottomRightRadius = "5px";
+  osccred.style.boxShadow = "0 0 5px #ccc";
+  document.body.appendChild(osccred);
+}
+
+defineProperty();
+
+const submitBtn = document.querySelector(".submit"),
+  nextBtn = document.querySelector(".next");
+submitBtn.addEventListener("click", () => {
+  checkAnswer();
+});
+
+nextBtn.addEventListener("click", () => {
+  nextQuestion();
+  submitBtn.style.display = "block";
+  nextBtn.style.display = "none";
+});
+
+const checkAnswer = () => {
+  clearInterval(timer);
+  const selectedAnswer = document.querySelector(".answer.selected");
+  if (selectedAnswer) {
+    const answer = selectedAnswer.querySelector(".text").innerHTML;
+    console.log(currentQuestion);
+    if (answer === questions[currentQuestion - 1].correct_answer) {
+      score++;
+      selectedAnswer.classList.add("correct");
+    } else {
+      selectedAnswer.classList.add("wrong");
+      const correctAnswer = document
+        .querySelectorAll(".answer")
+        .forEach((answer) => {
+          if (
+            answer.querySelector(".text").innerHTML ===
+            questions[currentQuestion - 1].correct_answer
+          ) {
+            answer.classList.add("correct");
+          }
+        });
+    }
+  } else { 
+    const correctAnswer = document
+      .querySelectorAll(".answer")
+      .forEach((answer) => {
+        if (
+          answer.querySelector(".text").innerHTML ===
+          questions[currentQuestion - 1].correct_answer
+        ) {
+          answer.classList.add("correct");
+        }
+      });
+  }
+  const answersDiv = document.querySelectorAll(".answer");
+  answersDiv.forEach((answer) => {
+    answer.classList.add("checked");
+  });
+
+  submitBtn.style.display = "none";
+  nextBtn.style.display = "block";
+};
+
+const nextQuestion = () => {
+  if (currentQuestion < questions.length) {
+    currentQuestion++;
+    showQuestion(questions[currentQuestion - 1]);
+  } else {
+    showScore();
+  }
+};
+
+const endScreen = document.querySelector(".end-screen"),
+  finalScore = document.querySelector(".final-score"),
+  totalScore = document.querySelector(".total-score");
+const showScore = () => {
+  endScreen.classList.remove("hide");
+  quiz.classList.add("hide");
+  finalScore.innerHTML = score;
+  console.log("Testing final score :  ",finalScore.innerHTML);
+  totalScore.innerHTML = `/ ${questions.length}`;
+
+  updateLeaderboard();
+};
+
+const restartBtn = document.querySelector(".restart");
+restartBtn.addEventListener("click", () => {
+  window.location.reload();
+});
+
+const playAdudio = (src) => {
+  const audio = new Audio(src);
+  audio.play();
+};
+
+
+function loadQuiz() {
+  var studentSelect = document.getElementById("student-select");
+  var quizSelect = document.getElementById("quiz-select");
+
+  
+  if (studentSelect.value === "1") {
+    
+    quizSelect.value = "Quiz 1";
+  }
+
+  else if(studentSelect.value === "2") {
+    
+    quizSelect.value = "Quiz 2";
+  }
+
+  else if (studentSelect.value === "3") {
+   
+    quizSelect.value = "Quiz 3";
+  }
+}
+document.getElementById("student-select").onchange = loadQuiz;
+
+// Code for leaderboard functionality
+function updateLeaderboard() {
+var leaderboardList = document.getElementById("leaderboard-list");
+
+leaderboardList.innerHTML = "";
+let studentNum=document.getElementById("student-select").value;
+
+if(studentNum=="1"){
+var leaderboardData = [{name: "Student A", score: score}];
+console.log("Student one selected")
+}else if(studentNum=="2"){
+var leaderboardData = [{name: "Student B", score: score}];
+
+console.log("Student two selected")
+}else if(studentNum=="3"){
+var leaderboardData = [{name: "Student C", score: score}];
+
+console.log("Student three selected")
+}
+
+
+// var leaderboardData = [
+//   { name: "Student A", score: score},
+//   { name: "Student B", score: 0},
+//   { name: "Student C", score: 0},
+//   // Add more data as needed
+// ];
+
+leaderboardData.sort((a, b) => b.score - a.score);
+
+leaderboardData.forEach((item, index) => {
+var listItem = document.createElement("li");
+listItem.innerHTML = `${index + 1}. ${item.name} - ${item.score}`;
+leaderboardList.appendChild(listItem);
+});
+}
+
+document.getElementById("update-leaderboard-button").addEventListener("click", updateLeaderboard);
+
+// Call updateLeaderboard initially and whenever needed
+updateLeaderboard();
+
+
